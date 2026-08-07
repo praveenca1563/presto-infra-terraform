e9b4e3f296196cce72bbb15ed21127a3d4520d02
Praveen <Nicey1563@hotmail.com>
2026-08-07 09:36:16 +0530

    Fix Databricks no_public_ip configuration

--- modules/databricks/main.tf
+++ modules/databricks/main.tf
@@ -8,7 +8,7 @@ resource "azurerm_databricks_workspace" "this" {
 
   # public_network_access_enabled = var.public_network_access_enabled
   public_network_access_enabled = false
-  no_public_ip                  = true
+  
 
   dynamic "custom_parameters" {
     for_each = var.enable_vnet_injection ? [1] : []
@@ -19,7 +19,8 @@ resource "azurerm_databricks_workspace" "this" {
       private_subnet_name                                  = var.private_subnet_name
       public_subnet_network_security_group_association_id  = var.public_subnet_nsg_association_id
       private_subnet_network_security_group_association_id = var.private_subnet_nsg_association_id
-      #  no_public_ip                                         = var.no_public_ip
+      #  no_public_ip                                      = var.no_public_ip
+      no_public_ip                                         = true
     }
   }
 
