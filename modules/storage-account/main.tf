3c782f3c45fff18d61730c5a8c8b14b4ce80fc75
Praveen <Nicey1563@hotmail.com>
2026-08-21 19:31:26 +0530

    Fix storage blob data plane permissions

--- modules/storage-account/main.tf
+++ modules/storage-account/main.tf
@@ -1,63 +1,72 @@
 resource "azurerm_storage_account" "this" {
-  name                          = var.storage_account_name
-  resource_group_name           = var.resource_group_name
-  location                      = var.location
-  account_tier                  = var.account_tier
-  account_replication_type      = var.replication_type
-  account_kind                  = "StorageV2"
-  is_hns_enabled                = true # required for ADLS Gen2
-  min_tls_version               = "TLS1_2"
+  name                     = var.storage_account_name
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = var.account_tier
+  account_replication_type = var.replication_type
+
+  account_kind = "StorageV2"
+
+  is_hns_enabled = true
+
+  min_tls_version           = "TLS1_2"
+  https_traffic_only_enabled = true
+
   public_network_access_enabled = var.public_network_access_enabled
 
   network_rules {
-    default_action             = var.network_default_action
+    default_action = var.network_default_action
+    ip_rules       = var.allowed_ip_ranges
     virtual_network_subnet_ids = var.allowed_subnet_ids
-    ip_rules                   = var.allowed_ip_ranges
   }
 
   blob_properties {
-    dynamic "delete_retention_policy" {
-      for_each = var.blob_soft_delete_days > 0 ? [1] : []
-      content {
-        days = var.blob_soft_delete_days
-      }
+    delete_retention_policy {
+      days = var.blob_soft_delete_days
     }
   }
 
   tags = var.tags
 }
 
+resource "azurerm_storage_container" "containers" {
+  for_each = toset(var.containers)
 
-# Emulates a "folder" inside a container (e.g. gold/data_management) by writing a
-# zero-byte placeholder blob, since ADLS containers have no native empty-folder concept.
+  name                  = each.value
+  storage_account_id    = azurerm_storage_account.this.id
+  container_access_type = "private"
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_role_assignment" "terraform_blob_data_contributor" {
+  scope                = azurerm_storage_account.this.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = data.azurerm_client_config.current.object_id
+
+  depends_on = [
+    azurerm_storage_account.this
+  ]
+}
 
 resource "azurerm_storage_blob" "folders" {
-  for_each = { for f in var.folders : "${f.container}/${f.path}" => f }
+  for_each = {
+    for f in var.folders :
+    "${f.container}/${f.path}" => f
+  }
 
   name = "${each.value.path}/.keep"
+
   storage_container_id = azurerm_storage_container.containers[
     each.value.container
   ].id
-
-  # storage_account_name   = azurerm_storage_account.this.name
-  # storage_container_name = each.value.container
 
   type           = "Block"
   source_content = ""
 
   depends_on = [
     azurerm_storage_container.containers,
+    azurerm_role_assignment.terraform_blob_data_contributor,
     var.rbac_dependency
   ]
-}
-
-resource "azurerm_storage_container" "containers" {
-
-  for_each = toset(var.containers)
-
-  name                  = each.value
-  storage_account_id    = azurerm_storage_account.this.id
-  container_access_type = "private"
-
-
 }
